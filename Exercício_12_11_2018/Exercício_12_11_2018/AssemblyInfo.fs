08f664cb27d9b127ebb6a2a88e1a4fd1d4e74d9c
Eduardo da Silva Lamim <eduardo.lamim03@gmail.com>
2018-11-12 23:04:16 -0200

    Exercícios adicionados do dia 12/11/2018

---
+++ Exercício_12_11_2018/Exercício_12_11_2018/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Exercício_12_11_2018.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Exercício_12_11_2018")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Exercício_12_11_2018")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("ec5d6ac6-f427-4a95-9658-1c2bd5e6843c")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Exercício_12_11_2018/Exercício_12_11_2018/Program.fs
@@ -0,0 +1,111 @@
+﻿open System
+
+let Exercicio01() =
+
+    Console.WriteLine "Exercício 1"
+    Console.WriteLine ""
+
+    let lista = [0 ; 1 ; 2 ; 3 ; 4 ; 5]
+    let elevarLista = List.map (fun num -> num * num) lista
+
+    Console.WriteLine "Lista com os números ao quadrado:"
+    Console.WriteLine ""
+
+    for n in elevarLista do 
+        Console.Write(n)
+        Console.Write "\t"
+
+let Exercicio02() = 
+    
+    Console.WriteLine ""
+    Console.WriteLine ""
+    Console.WriteLine "-------------------------------------------"
+    Console.WriteLine ""
+    Console.WriteLine "Exercício 2"
+    Console.WriteLine ""
+
+    let listaNomes = ["Eduardo" ; "Wagner" ; "Tamara" ; "Bruna" ; "João"]
+    
+    let teste = List.filter (fun (nomes:string) -> nomes.StartsWith "T") listaNomes
+
+    Console.WriteLine "Nomes da lista que começam com a letra T:"
+    Console.WriteLine ""
+
+    for n in teste do
+        Console.WriteLine(n)
+        Console.WriteLine "\t"
+  
+
+let Exercicio03() =
+
+    Console.WriteLine ""
+    Console.WriteLine "-------------------------------------------"
+    Console.WriteLine ""
+    Console.WriteLine "Exercício 3"
+    Console.WriteLine ""
+    
+    let lista = [1..6]
+
+    let multiplicarLista = List.reduce (fun acumulador num -> acumulador * num) lista
+
+    Console.WriteLine ""
+    Console.Write "Multiplicação de todos os números da lista: "
+    Console.WriteLine(multiplicarLista)
+    Console.WriteLine ""
+
+let Exercicio04() =
+
+    Console.WriteLine "-------------------------------------------"
+    Console.WriteLine ""
+    Console.WriteLine "Exercício 4"
+    Console.WriteLine ""
+
+    let lista = [1..10]
+
+    Console.Write "Digite um número: "
+    let n = Console.ReadLine()
+
+    let numerosPares = List.filter (fun num -> num % 2 = 0) lista
+    let numerosMaioresN = List.filter (fun num -> num > (int)n) lista
+
+ 
+
+    Console.WriteLine ""
+    
+    Console.WriteLine "Números pares:"
+    Console.WriteLine ""
+    for n in numerosPares do
+        Console.Write(n)
+        Console.Write "\t"
+    
+    Console.WriteLine ""
+    Console.WriteLine ""
+
+    Console.Write "Números maiores que "
+    Console.Write n
+    Console.WriteLine ":"
+    Console.WriteLine ""
+
+    for n in numerosMaioresN do
+        Console.Write(n)
+        Console.Write "\t"
+
+    Console.WriteLine ""
+    Console.WriteLine ""
+    Console.WriteLine "-------------------------------------------"
+
+[<EntryPoint>]
+let main argv = 
+   
+   Console.WriteLine "Eduardo da Silva Lamim"
+   Console.WriteLine ""
+
+   Exercicio01()
+   Exercicio02()
+   Exercicio03()
+   Exercicio04()
+
+   Console.ReadKey()
+    |> ignore
+
+   0
